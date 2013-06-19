28056b2155212ae8dbb63f174e70db82c131166a
Michael Hartl <michael@michaelhartl.com>
2013-06-19 11:06:36 -0700

    Get pull requests working
    [Delivers #51952769]

---
+++ lib/git-utils/pull_request.rb
@@ -0,0 +1,46 @@
+require 'git-utils/command'
+
+class PullRequest < Command
+
+  def parser
+    OptionParser.new do |opts|
+      opts.banner = "Usage: git pull-request"
+      opts.on_tail("-h", "--help", "this usage guide") do
+        puts opts.to_s; exit 0
+      end
+    end
+  end
+
+  # Returns the URL for a new pull request.
+  def new_pr_url
+    if service == 'stash' && protocol == 'ssh'
+      pattern = /(.*)@([^:]*):?([^\/]*)\/([^\/]*)\/(.*)\.git/
+      replacement = 'https://\2/projects/\4/repos/\5/pull-requests?create&sourceBranch=' +
+                    current_branch
+    elsif service == 'stash' && protocol == 'http'
+      pattern = /(.*)@([^:\/]*)(:?[^\/]*)\/(.*)scm\/([^\/]*)\/(.*)\.git/
+      replacement = 'https://\2\3/\4projects/\5/repos/\6/pull-requests?create&sourceBranch=' +
+                    current_branch
+    elsif service == 'github' && protocol == 'ssh'
+      pattern = /(.*)@(.*):(.*)\.git/
+      replacement = 'https://\2/\3/pull/new/' + current_branch
+    elsif service == 'github' && protocol == 'http'
+      pattern = /https?\:\/\/(([^@]*)@)?(.*)\.git/
+      replacement = 'https://\3/pull/new/' + current_branch
+    elsif service == 'bitbucket' && protocol == 'ssh'
+      pattern = /(.*)@(.*):(.*)\.git/
+      replacement = 'https://\2/\3/pull-request/new/'
+    elsif service == 'bitbucket' && protocol == 'http'
+      pattern = /https?\:\/\/(([^@]*)@)?(.*)\.git/
+      replacement = 'https://\3/pull-request/new/'
+    end
+    origin_url.sub(pattern, replacement)
+  end
+
+  # Returns a command appropriate for executing at the command line
+  def cmd
+    c = ["open #{new_pr_url}"]
+    c << argument_string(unknown_options) unless unknown_options.empty?
+    c.join("\n")
+  end
+end
---
+++ spec/commands/pull_request_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe PullRequest do
+
+  let(:command) { PullRequest.new }
+  before do
+    command.stub(:current_branch).and_return('test-br')
+  end
+  subject { command }
+
+  its(:cmd) { should match /open #{command.new_pr_url}/ }
+
+  it "should have the right pull request URLs" do
+    urls = %w[
+https://mwatson@bitbucket.org/atlassian/amps.git https://bitbucket.org/atlassian/amps/pull-request/new
+git@bitbucket.org:atlassian/amps.git https://bitbucket.org/atlassian/amps/pull-request/new
+git@github.com:mhartl/git-utils.git https://github.com/mhartl/git-utils/pull/new/test-br
+https://github.com/mhartl/git-utils.git https://github.com/mhartl/git-utils/pull/new/test-br
+ssh://git@stash.atlassian.com:7999/stash/stash.git https://stash.atlassian.com/projects/stash/repos/stash/pull-requests?create&sourceBranch=test-br
+https://mwatson@stash.atlassian.com:7990/scm/stash/stash.git https://stash.atlassian.com:7990/projects/stash/repos/stash/pull-requests?create&sourceBranch=test-br
+ssh://git@stash.atlassian.com/stash/stash.git https://stash.atlassian.com/projects/stash/repos/stash/pull-requests?create&sourceBranch=test-br
+https://mwatson@stash.atlassian.com/scm/stash/stash.git https://stash.atlassian.com/projects/stash/repos/stash/pull-requests?create&sourceBranch=test-br
+https://mwatson@stash.atlassian.com/stash/scm/stash/stash.git https://stash.atlassian.com/stash/projects/stash/repos/stash/pull-requests?create&sourceBranch=test-br
+https://mwatson@stash.atlassian.com:7990/stash/scm/stash/stash.git https://stash.atlassian.com:7990/stash/projects/stash/repos/stash/pull-requests?create&sourceBranch=test-br
+    ]
+
+    urls.each_slice(2) do |origin_url, new_pr_url|
+      command.stub(:origin_url).and_return(origin_url)
+      expect(command.new_pr_url).to include new_pr_url
+    end
+  end
+
+
+  describe "command-line command" do
+    subject { `bin/git-pull-request --debug` }
+    it { should match /open/ }
+  end
+end
