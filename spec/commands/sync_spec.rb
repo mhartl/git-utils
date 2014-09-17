efbe898857d7d70ecb8aaedf6ba4570972facde7
Michael Hartl <michael@michaelhartl.com>
2014-09-17 08:52:33 -0700

    Add sync test for alternate branch

--- spec/commands/sync_spec.rb
+++ spec/commands/sync_spec.rb
@@ -12,6 +12,12 @@ describe Sync do
   its(:cmd) { should match /git pull/ }
   its(:cmd) { should match /git checkout #{command.current_branch}/ }
 
+  describe "description" do
+    let(:alternate_branch) { 'alternate' }
+    before { command.stub(:known_options).and_return([alternate_branch]) }
+    its(:cmd) { should match /git checkout #{alternate_branch}/ }
+  end
+
   describe "command-line command" do
     subject { `bin/git-sync --debug` }
     it { should match /git checkout master/ }
